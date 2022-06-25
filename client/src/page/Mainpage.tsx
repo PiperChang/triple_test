7d4ea94ee22ae9f84e87c10496e67da8016fcbee
PiperChang <goddhqj8000@ajou.ac.kr>
2022-06-25 23:55:58 +0900

    Add : animation

--- client/src/page/Mainpage.tsx
+++ client/src/page/Mainpage.tsx
@@ -1,12 +1,27 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { useRef } from 'react'
 
 import logo from '../assets/triple2x.png'
 import googlePlayStore from '../assets/play-store2x.png'
 import appleAppStore from '../assets/badge-apple4x.png'
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
 const SectionContainer = styled.div`
   position: relative;
   overflow: hidden;
+  .fadeIn {
+    opacity: 0;
+    animation-name: ${fadeIn};
+    animation-duration: 700ms;
+    animation-fill-mode: forwards;
+  }
 `
 const Section = styled.section`
   width: 1040px;
@@ -34,6 +49,7 @@ const ContentLogo = styled.div`
 const MetricsContainer = styled.div`
   margin-left: 623px;
   padding-top: 150px;
+  animation-delay: 100ms;
   div {
     font-size: 36px;
     letter-spacing: -1px;
@@ -43,15 +59,18 @@ const MetricsContainer = styled.div`
     font-weight: bold;
   }
 `
-interface AwardItemProps {
-  src: string
-}
 
 const AwardsContainer = styled.div`
   margin-left: 623px;
   margin: 50px 0px 140px 623px;
   white-space: nowrap;
+  animation-delay: 200ms;
 `
+
+interface AwardItemProps {
+  src: string
+}
+
 const AwardsItem = styled.div<AwardItemProps>`
   display: inline-block;
   padding: 5px 0px 5px 62px;
@@ -66,33 +85,51 @@ const AwardsItem = styled.div<AwardItemProps>`
   background-repeat: no-repeat;
   background-size: 54px 54px;
 `
-
 const Mainpage = () => {
+  const travelerRef = useRef<HTMLInputElement>(null)
+  const reviewRef = useRef<HTMLInputElement>(null)
+  const scheduleRef = useRef<HTMLInputElement>(null)
+  const refList = [travelerRef, reviewRef, scheduleRef]
+  refList.forEach((ref) => {
+    let now = 0
+    let percent = 1
+    if (ref.current !== null) {
+      const maxNum = parseInt(ref.current.innerHTML)
+      setInterval(() => {
+        now = Math.ceil(maxNum * (1 - percent))
+        percent = percent / 2
+        if (ref.current !== null) {
+          ref.current.innerHTML = now.toString()
+        }
+      }, 250)
+    }
+  })
+
   return (
     <SectionContainer>
       <Section>
-        <ContentLogo>2021년 12월 기준</ContentLogo>
-        <MetricsContainer>
+        <ContentLogo className="fadeIn">2021년 12월 기준</ContentLogo>
+        <MetricsContainer className="fadeIn">
           <div>
             <strong>
-              <span>700</span>만 명
+              <span ref={travelerRef}>700</span>만 명
             </strong>
             의 여행자
           </div>
           <div>
             <strong>
-              <span>100</span>만 개
+              <span ref={reviewRef}>100</span>만 개
             </strong>
             의 여행 리뷰
           </div>
           <div>
             <strong>
-              <span>470</span>만 개
+              <span ref={scheduleRef}>470</span>만 개
             </strong>
             의 여행 일정
           </div>
         </MetricsContainer>
-        <AwardsContainer>
+        <AwardsContainer className="fadeIn">
           <AwardsItem src={googlePlayStore}>
             2018 구글 플레이스토어
             <br />
